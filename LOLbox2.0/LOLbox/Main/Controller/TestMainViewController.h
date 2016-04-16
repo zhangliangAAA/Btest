5ad1fa34140e3e51cc3c6ca223c8c93117827521
zhanglianghao <zhanglianghao@zhanglianghaodeMacBook-Pro.local>
2016-04-16 13:14:25 +0800

    我是main 增加VC

---
+++ LOLbox2.0/LOLbox/Main/Controller/TestMainViewController.h
@@ -0,0 +1,13 @@
+//
+//  TestMainViewController.h
+//  LOLbox
+//
+//  Created by zhanglianghao on 16/4/16.
+//  Copyright © 2016年 lanou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TestMainViewController : UIViewController
+
+@end
---
+++ LOLbox2.0/LOLbox/Main/Controller/TestMainViewController.m
@@ -0,0 +1,38 @@
+//
+//  TestMainViewController.m
+//  LOLbox
+//
+//  Created by zhanglianghao on 16/4/16.
+//  Copyright © 2016年 lanou. All rights reserved.
+//
+
+#import "TestMainViewController.h"
+
+@interface TestMainViewController ()
+
+@end
+
+@implementation TestMainViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    NSLog(@"我也增加VC");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
